d3ff501846e36f67cd8b0f9932f925dae4c441de
Eric Lin <132201101+Supericlin@users.noreply.github.com>
2025-03-14 20:56:10 +0800

    update files

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const App = () => {
+
+  const navigation = useNavigation();
+  
+  const handleLogin = () => {
+    // Navigate to Home screen
+    navigation.navigate("Home");
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login Form</Text>
       <TextInput style={styles.input} placeholder='Email'/>
-      <TextInput style={styles.input} placeholder='Password'/>
-      <TouchableOpacity> </TouchableOpacity>
+      <TextInput style={styles.input} placeholder='Password' secureTextEntry />
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -36,6 +46,19 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
   },
+  buttonText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  button: {
+    width: '80%',
+    height: 50,
+    backgroundColor: '#ed9d2d',
+    borderRadius: 8,
+    padding: 10,
+    alignItems: 'center',
+  }
 });
 
 export default App;
